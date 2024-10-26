a09c130fd42e830cc393fac063a48b232f4cf7bf
C0de_Crusaders <hizolajohncarlo2004@gmail.com>
2024-10-26 20:13:10 +0800

    test 2 routing

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-const dev = process.env.NODE_ENV === 'development';
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
@@ -15,7 +13,7 @@ const config = {
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({ pages: "build", assets: "build", fallback: "index.html", precompress: false, strict: true}),
 		paths: {
-			base: dev ? '' : '/project2'
+			base: '/project2'
 		}
 	}
 };
